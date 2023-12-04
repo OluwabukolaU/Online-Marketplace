94c4a7850fbff1e8959f03cad7068ba6fbaa5da8
OluwabukolaU <oluwabukola.umaru@gmail.com>
2023-12-04 13:05:41 +0100

    hosting on vercel

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -10,3 +10,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),   
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
